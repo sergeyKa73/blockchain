12d7c69a0cac91f4ca6db830ea46f0443ec2fc3a
Sergey <karandeev73@gmail.com>
2021-11-23 21:10:41 +0300

    feat: add check hash

--- block.py
+++ block.py
@@ -2,24 +2,45 @@ import json
 import os
 import hashlib
 
+blockchain_dir = os.curdir + '/files/'
+
+
 def get_hash(filename):
-    blockchain_dir = os.curdir + '/files/'
     file = open(blockchain_dir + filename, 'rb').read()
 
-    return  hashlib.md5(file).hexdigest()
+    return hashlib.md5(file).hexdigest()
+
+
+def get_files():
+    files = os.listdir(blockchain_dir)
+    return sorted([int(i) for i in files])
+
 
 def chek_integrity():
-    pass
+    files = get_files()
+
+    for file in files[1:]:
+        f = open(blockchain_dir + str(file))
+        h = json.load(f)['hash']
+
+        prev_file = str(file - 1)
+        actual_hash = get_hash(prev_file)
+
+        if h == actual_hash:
+            res = 'OK'
+        else:
+            res = 'Corrupted'
+
+        print(f'block {prev_file} is: {res}')
+
 
 def write_block(name, amount, to_whom, prev_hash=''):
-    blockchain_dir = os.curdir + '/files/'
-    files = os.listdir(blockchain_dir)
-    files = sorted([int(i) for i in files])
+    files = get_files()
 
-    last_file = files[-1]
-    filename = str(last_file + 1)
+    prev_file = files[-1]
+    filename = str(prev_file + 1)
 
-    prev_hash = get_hash(str(last_file))
+    prev_hash = get_hash(str(prev_file))
 
     data = {'name': name,
             'amount': amount,
@@ -31,7 +52,9 @@ def write_block(name, amount, to_whom, prev_hash=''):
 
 
 def main():
-    write_block(name='maks', amount=5, to_whom='anna')
+    # write_block(name='maks', amount=5, to_whom='anna')
+    chek_integrity()
+
 
 if __name__ == '__main__':
     main()
